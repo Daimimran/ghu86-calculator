74d26aed906d83c85fcf457d59407d6d7311e290
FaizanAtif <159905173+FaizanAtif@users.noreply.github.com>
2024-08-09 04:55:25 -0700

    Add files via upload

---
+++ day_2.ts
@@ -0,0 +1,7 @@
+var myName = ("Faizan Atif");
+
+var paragraph = ("The morning sun cast a warm glow across the tranquil meadow"); //redeclare
+
+ myName = ("Faizy"); //reassign
+
+console.log(myName);
